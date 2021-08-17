47a49d2edc89297fd8a4b1cb52a79b08a4a5a64e
shreelekhabatabyal <81627770+shreelekhabatabyal@users.noreply.github.com>
2021-08-17 21:54:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var fname = "Shreelekha";
+var lname = "batabyal"
+console.log(fname.length);
+console.log(fname.toLowerCase());
+console.log(fname.toUpperCase());
+console.log(fname.concat(lname));
+fname = (fname.replace("Shreelekha", "Bandna"));
+console.log(fname.concat(lname));
+
+var x = 10;
+var y = 20;
+var z = Math.max(x, y);
+console.log(z);
